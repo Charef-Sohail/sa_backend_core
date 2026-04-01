17486e8f5f1b62b2e5afba4ae1fe4362c25afe43
yassine-fanidi <yassinefanidi55@gmail.com>
2026-04-01 22:26:04 +0100

    First Commit

---
+++ sa_backend_core/src/main/java/org/piteam/sa_backend_core/SaBackendCoreApplication.java
@@ -0,0 +1,13 @@
+package org.piteam.sa_backend_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaBackendCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaBackendCoreApplication.class, args);
+    }
+
+}
---
+++ sa_backend_core/src/test/java/org/piteam/sa_backend_core/SaBackendCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piteam.sa_backend_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaBackendCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
